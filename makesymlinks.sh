0370b1de7f9cdc31cd2894a59e8e20753aedaa3b
icalvin102 <calvin@icalvin.de>
2019-04-20 16:18:55 +0200

    added backup and cleanup to makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
+#title		:makesymlinks.sh
+#description	:create symlinks to the dotfiles in this repository and back up old .files
+#author		:icalvin102
 
-backupdir=~/.dotfile_backup/$(date +"%Y%m%d-%H%M%S")
+backupdir=~/.dotfiles_backup/$(date +"%Y%m%d-%H%M%S")
 counter=0
 
-for filepath in ./dotfiles/*; do
-	filename=$(basename $filename)
-	if [ "$filename" -ne "." ] && [ "$filename" -ne ".." ] ; then
-		echo $filename
-		if [ -e ~/$filename ] ; then
-			echo exists
+mkdir -p $backupdir
+
+for filepath in ./dotfiles/.*; do
+	filename=$(basename "$filepath")
+	if [ "$filename" != "." ] && [ "$filename" != ".." ] ; then
+		echo "$filename"
+		if [ -f ~/$filename ] || [ -d ~/$filename ] && [ ! -L ~/$filename ] ; then
+			echo "backing up $filename"
+			mv ~/$filename $backupdir/$filename
+			((var++))
 		fi
-
-
-
+		echo "linking $(realpath $filepath) to ~/$filename"
+		ln -sf $(realpath $filepath) ~/$filename
 		fi	      
+	done
+
+echo "cleaning up"
+rmdir --ignore-fail-on-non-empty $backupdir
+rmdir --ignore-fail-on-non-empty ~/.dotfiles_backup
+echo "finished"
+
