6c60843cc806f39b43ebe9b14cc89838dd18eea9
icalvin102 <calvin@icalvin.de>
2024-09-27 00:09:30 +0200

    feat: enhance lsp keymaps

--- nvim/.config/nvim/lua/user/lsp.lua
+++ nvim/.config/nvim/lua/user/lsp.lua
@@ -48,28 +48,30 @@ return function()
 		vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
 		-- Mappings.
-		local opts = { noremap = true, silent = true, buffer = bufnr }
+		local tb = require("telescope.builtin")
+
+		local function mk_opts(desc)
+			return { noremap = true, silent = true, buffer = bufnr, desc = desc }
+		end
 
 		-- See `:help vim.lsp.*` for documentation on any of the below functions
-		vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-		vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-		vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-		vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-		vim.keymap.set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-		vim.keymap.set("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-		vim.keymap.set("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-		vim.keymap.set("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-		vim.keymap.set("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-		vim.keymap.set("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-		vim.keymap.set("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-		vim.keymap.set("v", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-		vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-		vim.keymap.set("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-		vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-		vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-		vim.keymap.set("n", "<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-		vim.keymap.set("n", "<space>f", format, opts)
-		vim.keymap.set("v", "<space>f", format, opts)
+		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, mk_opts("Signature Help"))
+		vim.keymap.set("n", "<leader>D", tb.lsp_type_definitions, mk_opts("Type [D]efinition"))
+		vim.keymap.set("n", "<leader>ds", tb.lsp_document_symbols, mk_opts("[D]ocument [S]ymbols"))
+		vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, mk_opts("Show [E]rror"))
+		vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, mk_opts("Open Diagnostics List"))
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, mk_opts("[R]e[n]ame"))
+		vim.keymap.set("n", "<leader>ws", tb.lsp_dynamic_workspace_symbols, mk_opts("[W]orkspace [S]ymbols"))
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, mk_opts("Hover Documentation"))
+		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, mk_opts("Go to Previous Diagnostic"))
+		vim.keymap.set("n", "]d", vim.diagnostic.goto_next, mk_opts("Go to Next Diagnostic"))
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, mk_opts("[G]oto [D]eclaration"))
+		vim.keymap.set("n", "gI", tb.lsp_implementations, mk_opts("[G]oto [I]mplementation"))
+		vim.keymap.set("n", "gd", tb.lsp_definitions, mk_opts("[G]oto [D]efinition"))
+		vim.keymap.set("n", "gr", tb.lsp_references, mk_opts("[G]oto [R]eferences"))
+		vim.keymap.set("v", "<leader>f", format, mk_opts("[F]ormat Selection"))
+		vim.keymap.set({ "n", "v", "x" }, "<leader>ca", vim.lsp.buf.code_action, mk_opts("[C]ode [A]ction"))
+		vim.keymap.set({ "n", "v" }, "<leader>f", format, mk_opts("[F]ormat"))
 	end
 
 	-- Setup
@@ -86,7 +88,7 @@ return function()
 			require("lspconfig")[server_name].setup({
 				capabilities = capabilities,
 				on_attach = on_attach,
-				settings = servers[server_name],
+				settings = servers[server_name] or {},
 				flags = {
 					debounce_text_changes = 150,
 				},
