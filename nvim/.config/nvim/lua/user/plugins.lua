14380b182011dcb2535815e0847a8a1d2dfff1fa
icalvin102 <calvin@icalvin.de>
2024-09-26 03:29:34 +0200

    feat: add codeium

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -55,6 +55,13 @@ plugins = {
     },
     config = require('user.cmp')
   },
+  {
+    'Exafunction/codeium.vim',
+    config = function ()
+      vim.g.codeium_no_map_tab = 1
+      vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+    end
+  },
 
   -- Tresitter
   {
